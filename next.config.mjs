07772337e9f67c375108122e51ae41a4b7a2d89a
Tim Rüdenauer <10296479+tim-the-arcane@users.noreply.github.com>
2024-04-16 18:01:17 +0200

    Install payloadcms

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,4 @@
+import { withPayload } from '@payloadcms/next/withPayload'
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin("./i18n/config.ts");
@@ -5,4 +6,4 @@ const withNextIntl = createNextIntlPlugin("./i18n/config.ts");
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default withNextIntl(nextConfig);
+export default withPayload(withNextIntl(nextConfig));
