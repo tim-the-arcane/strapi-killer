b05275669456a84eab4fe1e0a959d5c5d6ba61ad
Tim Rüdenauer <10296479+tim-the-arcane@users.noreply.github.com>
2024-04-19 17:02:09 +0200

    Add roles to Users collection

---
+++ migrations/20240419_150103.ts
@@ -0,0 +1,35 @@
+import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-postgres'
+
+export async function up({ payload }: MigrateUpArgs): Promise<void> {
+await payload.db.drizzle.execute(sql`
+
+DO $$ BEGIN
+ CREATE TYPE "enum_users_roles" AS ENUM('admin', 'editor', 'author', 'event-manager');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+
+CREATE TABLE IF NOT EXISTS "users_roles" (
+	"order" integer NOT NULL,
+	"parent_id" integer NOT NULL,
+	"value" "enum_users_roles",
+	"id" serial PRIMARY KEY NOT NULL
+);
+
+CREATE INDEX IF NOT EXISTS "users_roles_order_idx" ON "users_roles" ("order");
+CREATE INDEX IF NOT EXISTS "users_roles_parent_idx" ON "users_roles" ("parent_id");
+DO $$ BEGIN
+ ALTER TABLE "users_roles" ADD CONSTRAINT "users_roles_parent_id_users_id_fk" FOREIGN KEY ("parent_id") REFERENCES "users"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+`);
+
+};
+
+export async function down({ payload }: MigrateDownArgs): Promise<void> {
+await payload.db.drizzle.execute(sql`
+
+DROP TABLE "users_roles";`);
+
+};
--- payload/collections/Users.ts
+++ payload/collections/Users.ts
@@ -1,13 +1,46 @@
-import type { CollectionConfig } from 'payload/types'
+// import { isAdmin } from "@/payload/access/isAdmin";
+// import { isAdminOrSelf } from "@/payload/access/isAdminOrSelf";
+import type { CollectionConfig } from "payload/types";
 
 export const Users: CollectionConfig = {
-  slug: 'users',
+  slug: "users",
   admin: {
-    useAsTitle: 'email',
+    useAsTitle: "email",
+  },
+  access: {
+    // create: isAdmin,
+    // read: isAdminOrSelf,
+    // update: isAdminOrSelf,
+    // delete: isAdmin,
   },
   auth: true,
   fields: [
-    // Email added by default
-    // Add more fields as needed
+    {
+      name: "roles",
+      type: "select",
+      hasMany: true,
+      defaultValue: "editor",
+      options: [
+        {
+          label: "Admin",
+          value: "admin",
+        },
+        {
+          label: "Editor",
+          value: "editor",
+        },
+        {
+          label: {
+            en: "Author",
+            de: "Autor",
+          },
+          value: "author",
+        },
+        {
+          label: "Event Manager",
+          value: "event-manager",
+        },
+      ],
+    },
   ],
-}
+};
