4ae88733c372ed64a06403941cc551525ad6d4df
Tim Rüdenauer <10296479+tim-the-arcane@users.noreply.github.com>
2024-04-19 22:32:40 +0200

    Add blog template and index page

---
+++ app/(web)/[locale]/blog/layout.tsx
@@ -0,0 +1,36 @@
+import { useTranslations } from "next-intl";
+import { getTranslations } from "next-intl/server";
+
+export const generateMetadata = async () => {
+  const t = await getTranslations("pages.Blog");
+
+  return {
+    title: t("title"),
+    description: t("description"),
+  };
+};
+
+export default function BlogLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const t = useTranslations("pages.Blog");
+
+  return (
+    <div className="h-full container grid grid-cols-[1fr,minmax(250px,_auto)] gap-8">
+      <main>
+        <div className="bg-black text-white p-8 rounded-xl">
+          <div className="flex flex-col gap-4">
+            <h1 className="text-4xl">{t("title")}</h1>
+            <p>{t("description")}</p>
+          </div>
+        </div>
+
+        <div>{children}</div>
+      </main>
+
+      <aside className="h-full p-8 border bg-zinc-200 rounded-xl"></aside>
+    </div>
+  );
+}
--- app/(web)/[locale]/blog/page.tsx
+++ app/(web)/[locale]/blog/page.tsx
@@ -1,12 +1,49 @@
-export default async function BlogPage({
+import configPromise from "@payload-config";
+import { getTranslations } from "next-intl/server";
+import { getPayload } from "payload";
+
+export const dynamic = "force-dynamic";
+
+const getBlogPosts = async (locale: string) => {
+  const payload = await getPayload({ config: configPromise });
+  const posts = await payload.find<"posts">({
+    collection: "posts",
+    locale: locale as "de" | "en",
+  });
+
+  return posts.docs;
+};
+
+export default async function BlogIndexPage({
   params: { locale },
 }: {
   params: { locale: string };
 }) {
+  const posts = await getBlogPosts(locale);
+  const t = await getTranslations("pages.Blog");
+
   return (
-    <main>
-      <h1>Blog</h1>
-      <p>Posts will go here</p>
-    </main>
+    <div className="grid gap-4 pt-8">
+      {posts.length === 0 && <p>{t("noArticles")}</p>}
+
+      {posts.length > 0 && (
+        <>
+          {posts.map((post) => (
+            <article
+              key={post.id}
+              className="p-4 border rounded-xl flex flex-col gap-4"
+            >
+              <h1 className="text-4xl font-light">{post.title}</h1>
+              {post?.content_html && (
+                <div
+                  className="prose prose-zinc"
+                  dangerouslySetInnerHTML={{ __html: post.content_html }}
+                />
+              )}
+            </article>
+          ))}
+        </>
+      )}
+    </div>
   );
 }
