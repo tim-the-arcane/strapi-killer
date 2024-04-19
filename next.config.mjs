f70d0a558193e01173139204035618c5e2c011d9
Tim Rüdenauer <10296479+tim-the-arcane@users.noreply.github.com>
2024-04-19 09:03:54 +0200

    Add dockerfile

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
-import { withPayload } from '@payloadcms/next/withPayload'
+import { withPayload } from '@payloadcms/next/withPayload';
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin("./i18n/config.ts");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'standalone',
+};
 
 export default withPayload(withNextIntl(nextConfig));
